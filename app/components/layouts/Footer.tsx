1ca6b1d434bb0f0a51ff363343991f455142d4a8
Emmanuel Julius <emmanueljulius9102016@gmail.com>
2023-10-17 03:38:53 +0100

    Add footer

--- app/components/layouts/Footer.tsx
+++ app/components/layouts/Footer.tsx
@@ -4,13 +4,14 @@ import brandLogo from '@/public/assets/brandLogo.svg'
 import Image from 'next/image'
 import Link from 'next/link'
 import AppDownloadButton from '../shared/AppDownloadButton'
+import US from 'country-flag-icons/react/3x2/US'
 import { footerSocials, footerInfo, footerAbout, footerPartnership, footerUser } from '@/utils/footerNav'
 
 const Footer = () => {
     return (
         <footer className='hidden xl:block px-3 py-6'>
             <div className='container mx-auto'>
-                <section className='grid grid-cols-12 gap-5 items-center'>
+                <section className='grid grid-cols-12 gap-5 items-center py-6'>
                     {/* Socials */}
                     <div className='col-span-2'>
                         <Link href={"/"}>
@@ -32,7 +33,7 @@ const Footer = () => {
                     </div>
 
                     {/* About */}
-                    <div className='col-span-2'>
+                    <div className='col-span-2 text-center'>
                         <h3 className='font-bold'>About</h3>
                         <nav className='mt-2'>
                             <ul>
@@ -50,7 +51,7 @@ const Footer = () => {
                     </div>
 
                     {/* Partnership */}
-                    <div className='col-span-2'>
+                    <div className='col-span-2 text-center'>
                         <h3 className='font-bold'>Partnership</h3>
                         <nav className='mt-2'>
                             <ul>
@@ -68,7 +69,7 @@ const Footer = () => {
                     </div>
 
                     {/* Info */}
-                    <div className='col-span-2'>
+                    <div className='col-span-2 text-center'>
                         <h3 className='font-bold'>Information</h3>
                         <nav className='mt-2'>
                             <ul>
@@ -86,7 +87,7 @@ const Footer = () => {
                     </div>
 
                     {/* User */}
-                    <div className='col-span-2'>
+                    <div className='col-span-2 text-center'>
                         <h3 className='font-bold'>User</h3>
                         <nav className='mt-2'>
                             <ul>
@@ -104,22 +105,28 @@ const Footer = () => {
                     </div>
 
                     {/* Download App */}
-                    <div className='col-span-2'>
+                    <div className='col-span-2 text-center'>
                         <h3 className='font-bold'>Get app</h3>
-                        <div>
+                        <div className='flex flex-col '>
                             <AppDownloadButton
                                 store='App Store'
-                                className='bg-black text-white gap-4 mt-2'
+                                className='bg-black text-white gap-4 mt-2 justify-center w-48 mx-auto'
                             />
 
                             <AppDownloadButton
                                 store='Google Play'
-                                className='bg-black text-white gap-1 mt-3'
+                                className='bg-black text-white gap-1 mt-2 justify-center w-48 mx-auto'
                             />
                         </div>
                     </div>
                 </section>
-                <section></section>
+                <section className='bg-gray-100 py-3 px-3 flex justify-between'>
+                    <span className='block'>&copy; {new Date().getFullYear()} Ecommerce</span>
+                    <div className='flex items-center gap-2'>
+                        <US title="United States" className="w-5"/>
+                        <p>English</p>
+                    </div>
+                </section>
             </div>
         </footer>
     )
